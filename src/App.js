e53d3b10bb10dffdf54b1e4cd06c3bea9d14a4ad
Laurent Hirth <laurent.hirth.68@gmail.com>
2021-08-08 15:52:02 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -10,19 +10,36 @@ import E404 from './pages/E404'
 import Accomodation from './pages/Accomodation'
 
 class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { data: [] }
+	}
+
+	componentDidMount() {
+		this.getData()
+	}
+
+	async getData() {
+		const response = await fetch(
+			'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/ddbc00d0-faf0-401c-8cbb-23b9b791b41f/logements.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210808%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210808T093544Z&X-Amz-Expires=86400&X-Amz-Signature=cc7e631f0db12548d159544f6aca687fc094639731c0c071111de19080b83e3e&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22logements.json%22'
+		)
+		const data = await response.json()
+		this.setState({ data: data })
+	}
+
 	render() {
 		return (
 			<Fragment>
 				<Header />
 				<Switch>
 					<Route exact path="/">
-						<Home />
+						<Home data={this.state.data} />
 					</Route>
 					<Route path="/about">
 						<About />
 					</Route>
 					<Route path="/accomodations/:id">
-						<Accomodation />
+						<Accomodation data={this.state.data} />
 					</Route>
 					<Route path="*">
 						<E404 />
--- src/pages/Accomodation.js
+++ src/pages/Accomodation.js
@@ -28,17 +28,10 @@ class Accomodation extends React.Component {
 
 	componentDidMount() {
 		const id = this.props.match.params.id
-		this.getData(id)
-	}
-
-	// TODO: Injection de dépendance
-	async getData(id) {
-		const response = await fetch(
-			'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/ddbc00d0-faf0-401c-8cbb-23b9b791b41f/logements.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210808%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210808T093544Z&X-Amz-Expires=86400&X-Amz-Signature=cc7e631f0db12548d159544f6aca687fc094639731c0c071111de19080b83e3e&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22logements.json%22'
-		)
-		const data = await response.json()
 		this.setState({
-			accomodation: data.find((accomodation) => accomodation.id === id),
+			accomodation: this.props.data.find(
+				(accomodation) => accomodation.id === id
+			),
 		})
 	}
 
