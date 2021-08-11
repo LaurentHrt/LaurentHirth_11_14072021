254ece91343c06b80e8bc862bb435a9db3fbac76
Laurent Hirth <laurent.hirth.68@gmail.com>
2021-08-11 12:08:44 +0200

    carrousel OK

--- src/components/Carrousel/Carrousel.js
+++ src/components/Carrousel/Carrousel.js
@@ -1,15 +1,51 @@
 import React from 'react'
 import './Carrousel.css'
+import rightArrow from '../../assets/pictures/rightArrow.png'
+import leftArrow from '../../assets/pictures/leftArrow.png'
 
 class Carrousel extends React.Component {
-	render() {
-		const { images } = this.props
+	constructor(props) {
+		super(props)
+		this.state = { imageIndex: 0 }
+	}
 
+	onClickLeftArrow() {
+		let currentIndex = this.state.imageIndex
+		if (currentIndex === 0)
+			this.setState({ imageIndex: this.props.images.length - 1 })
+		else this.setState({ imageIndex: --currentIndex })
+	}
+
+	onClickRightArrow() {
+		let currentIndex = this.state.imageIndex
+		if (currentIndex === this.props.images.length - 1) {
+			this.setState({ imageIndex: 0 })
+		} else {
+			this.setState({ imageIndex: ++currentIndex })
+		}
+	}
+
+	render() {
+		const { imageIndex } = this.state
 		return (
 			<div className="carrousel">
-				{images.map((image, index) => (
-					<img key={index} src={image} alt={image} />
-				))}
+				<img
+					className="leftArrow"
+					src={leftArrow}
+					alt="left"
+					onClick={() => this.onClickLeftArrow()}
+				/>
+				<img
+					className="carrouselImages"
+					src={this.props.images[imageIndex]}
+					alt={this.props.images[imageIndex]}
+				/>
+				<img
+					className="rightArrow"
+					src={rightArrow}
+					alt="right"
+					onClick={() => this.onClickRightArrow()}
+				/>
 			</div>
 		)
 	}
